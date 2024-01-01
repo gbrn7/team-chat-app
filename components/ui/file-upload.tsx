d887cf67d289f7377b5ae606c25242a427e3da03
gbrn7 <mraygbrn@gmail.com>
2024-01-01 10:20:54 +0700

    feat(modal): created initial modal

--- components/ui/file-upload.tsx
+++ components/ui/file-upload.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import { UploadDropzone } from "@/lib/uploadthing";
-
+import { X } from "lucide-react";
+import Image from "next/image";
 import "@uploadthing/react/styles.css"
 
 //this bellow code for confirming the props in only in defined types in interface class
@@ -17,13 +18,31 @@ export const FileUpload = ({
   value,
   endpoint
 }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image
+          fill
+          src={value}
+          alt="Upload"
+          className="rounded-full "
+        />
+        <button onClick={() => onChange()} className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm" type="button">
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    )
+  }
   return (
     <UploadDropzone
       endpoint={endpoint}
       onClientUploadComplete={(res) => {
         onChange(res?.[0].url);
-      }
-      }
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
     />
   )
 }
