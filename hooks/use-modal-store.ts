09dc5111136ff8075343cecbe6654e780baa11c7
gbrn7 <mraygbrn@gmail.com>
2024-01-02 10:18:29 +0700

    feat(server): created update server setting fetaure

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Server } from '@prisma/client';
 import {create } from 'zustand'
 //this code like react redux state management state
-export type ModalType = "createServer" | "invite";
+export type ModalType = "createServer" | "invite" | "editServer";
 
 interface ModalData {
   server?: Server
