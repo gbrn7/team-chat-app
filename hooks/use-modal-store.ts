b3ac7f5de0e7f94937512c0ea95fe780efa74c91
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2024-07-03 09:10:11 +0700

    feat(component): add chatIteComponent

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Channel, ChannelType, Server } from '@prisma/client';
 import {create } from 'zustand'
 //this code like react redux state management state
-export type ModalType = "createServer" | "invite" | "editServer" | "members" |  "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel" | "messageFile";
+export type ModalType = "createServer" | "invite" | "editServer" | "members" |  "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel" | "messageFile" | "deleteMessage";
 
 interface ModalData {
   server?: Server,
